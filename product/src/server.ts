683d23dc2a351f5119d12cb9fafa1d57621a9d8c
sourav_allmysons <souravmajumdar.dev@gmail.com>
2023-08-15 21:50:37 +0530

    Setup product service

---
+++ product/src/server.ts
@@ -0,0 +1,30 @@
+import 'express-async-errors';
+import cors from 'cors';
+import cookieSession from 'cookie-session';
+import express, { NextFunction, Request, Response } from 'express';
+import { errorHandler, NotFoundError } from '@woo-cart/common';
+const app = express();
+
+app.set('trust proxy', true);
+
+app.use(
+  cookieSession({
+    signed: false,
+    // secure: process.env.NODE_ENV !== 'test',
+    secure: false,
+  })
+);
+app.use(express.json());
+app.use(cors());
+
+// app.use('/api/users', signUpRoute);
+// app.use('/api/users', loginUserRoute);
+// app.use('/api/users', currentUserRoute);
+
+app.use('*', (req: Request, res: Response, next: NextFunction) => {
+  return next(new NotFoundError(`${req.originalUrl} is not find to this server!`));
+});
+
+app.use(errorHandler);
+
+export { app };
